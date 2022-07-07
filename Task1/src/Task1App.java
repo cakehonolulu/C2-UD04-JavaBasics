ad799fd4b52d02670c66c27b2015fd9c2c65c880
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-07 10:59:36 +0200

    Upload the 5 tasks

---
+++ Task1/src/Task1App.java
@@ -0,0 +1,15 @@
+
+public class Task1App {
+
+	public static void main(String[] args) {
+		int m_num_a = 20;
+		int m_num_b = 5;
+		
+		System.out.println(m_num_a + " + " + m_num_b + " = " + (m_num_a + m_num_b));
+		System.out.println(m_num_a + " - " + m_num_b + " = " + (m_num_a - m_num_b));
+		System.out.println(m_num_a + " * " + m_num_b + " = " + (m_num_a * m_num_b));
+		System.out.println(m_num_a + " / " + m_num_b + " = " + (m_num_a / m_num_b));
+		System.out.println(m_num_a + " % " + m_num_b + " = " + (m_num_a % m_num_b));
+	}
+
+}
